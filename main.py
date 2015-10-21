28e9a3ba3e856f8e4419c001897bf99ce6adc045
khalidsakib <knsakib@gmail.com>
2015-10-21 02:52:54 -0400

    datastore show without ancestor query
    not update instantly

---
+++ main.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import logging
+import os
+
+import jinja2
+import webapp2
+
+from model import database1
+
+
+jinja_env = jinja2.Environment(
+  loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+  autoescape=True)
+
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        message_query=database1.query().order(-database1.last_touch_date_time)
+        
+        
+        template1= jinja_env.get_template('templates/message.html')
+        
+        self.response.out.write(template1.render({"message_query":message_query}))
+        
+class showMessages(webapp2.RequestHandler):
+    def get(self):
+        message_query=database1.query().order(-database1.last_touch_date_time)
+        
+        
+        template2= jinja_env.get_template('templates/all_the_messages.html')
+        
+        self.response.out.write(template2.render({"message_query":message_query}))
+        
+
+class saveMessage(webapp2.RedirectHandler):
+    def post(self):
+        logging.info(str(self.request))
+        obj_databasemodel=database1(email=self.request.get("email"),
+                                    message=self.request.get("message"))
+        obj_databasemodel.put()
+        self.redirect(self.request.referer)
+ 
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    ('/save_message', saveMessage),
+    
+], debug=True)
